f35567333760d5a1cfba5d04e65410aafe9e5198
khabibullet <irek.rifson@gmail.com>
2022-06-21 22:58:35 +0300

    pulling kek_main.c

---
+++ sources/main_my.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_my.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 20:03:07 by anemesis          #+#    #+#             */
+/*   Updated: 2022/06/21 22:37:28 by anemesis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minirt.h"
+
+// int	main(void)
+// {
+// 	t_vec		solved;
+// 	t_ray		ray;
+// 	t_sphere	sphere;
+
+// 	ray.coords = (t_vec){1, 1, 1};
+// 	ray.origin = (t_vec){1, 1, 1};
+
+// 	sphere.center = (t_vec){1, 1, 1};
+// 	sphere.radius = 0.1;
+
+// 	solved = encounter_sphere(ray, sphere);
+// 	if (isnan(solved.x) == FP_NAN)
+// 		printf("Intersection not found!\n");
+// 	else
+// 		printf("Intersection found!\n");
+// 	return (0);
+// }
