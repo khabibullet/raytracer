0f554f8ac7f8868310cf2f34ab190299a25f9c43
khabibullet <irek.rifson@gmail.com>
2022-06-21 23:21:42 +0300

    basic raytracing works

--- sources/kek_main.c
+++ sources/kek_main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   kek_main.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enoye <enoye@clown.ru>                     +#+  +:+       +#+        */
+/*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 18:27:00 by enoye             #+#    #+#             */
-/*   Updated: 2022/06/21 22:24:33 by enoye            ###   ########.fr       */
+/*   Updated: 2022/06/21 23:15:16 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,33 @@
 
 int	main(void)
 {
-	// void	*mlx;
-	// void	*win;
+	t_cam		cam;
+	float		focal;
+	t_ray		ray;
+	t_pix		pix;
+	float		ratio;
+	float		density;
+	t_vec		tmp;
+	t_sphere	sphere;
+	int			j;
+	int			i;
+	void		*mlx;
+	void		*win;
 
-	// mlx = mlx_init();
-	// win = mlx_new_window(mlx, 1000, 1000, "here");
-	// (void)win;
-	// // mlx_loop(mlx);
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 1000, 1000, "here");
+	(void)win;
 
-	t_cam	cam_dir;
-	float	focal;
-	t_ray	ray;
-	t_pix	pix;
-	float	ratio, density;
-	t_vec	tmp;
-	t_sphere;
-
-	cam.coords = (t_vec) {0, 0, 0};
-	cam.direction = (t_vec) {1, 0, 0};
+	sphere.center = (t_vec){4, 0, 0};
+	sphere.radius = 0.1;
+	cam.coords = (t_vec){0, 0, 0};
+	cam.direction = (t_vec){1, 0, 0};
 	cam.fov = 60;
 	focal = 1.73;
-	int i = 0;
 	density = 0.001;
 	ratio = 1;
 
+	i = 0;
 	while (i < 999)
 	{
 		j = 0;
@@ -45,14 +48,14 @@ int	main(void)
 		{
 			pix = find_pix_coords(i, j, density, ratio);
 			ray.coords = get_cam_to_pix_vec(cam.direction, focal, pix);
-			ray.origin = get_cam_to_pix_vec(cam.direction, focal, pix);
-			tmp = encouter_sphere(ray, spehere);
-			if (is_non(tmp.x) != FP_NON)
-				mlx_pixel_put(mlx, win, i, j, 0x000000000);
+			ray.origin = ray.coords;
+			tmp = encounter_sphere(ray, sphere);
+			if (isnan(tmp.x) != FP_NAN)
+				mlx_pixel_put(mlx, win, i, j, 0x00FFFFFF);
 			j++;
 		}
 		i++;
 	}
-
+	mlx_loop(mlx);
 	return (0);
 }
