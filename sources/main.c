b43dc0429b3931073a697175cc418d9120bc2420
khabibullet <irek.rifson@gmail.com>
2022-06-21 13:20:47 +0300

    adding 42 headers

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: anemesis <anemesis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 15:12:07 by anemesis          #+#    #+#             */
-/*   Updated: 2022/06/20 12:58:26 by anemesis         ###   ########.fr       */
+/*   Updated: 2022/06/21 12:49:20 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ int	main(void)
 {
 	void	*mlx;
 	void	*win;
+	float	a;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 1000, 1000, "here");
 	(void)win;
-	mlx_loop(mlx);
+	// mlx_loop(mlx);
+	a = -10;
+	printf("%d\n", isnan(sqrtf(a)));
 	return (0);
 }
