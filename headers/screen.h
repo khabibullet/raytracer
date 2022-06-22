2046a2fdb8df69dbf0bd95b3128556076fffe4d5
khabibullet <irek.rifson@gmail.com>
2022-06-22 23:05:12 +0300

    building MVP raw raytracing binary

--- headers/screen.h
+++ headers/screen.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   screen.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enoye <enoye@clown.ru>                     +#+  +:+       +#+        */
+/*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 12:55:07 by enoye             #+#    #+#             */
-/*   Updated: 2022/06/21 15:43:16 by enoye            ###   ########.fr       */
+/*   Updated: 2022/06/22 15:46:18 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,16 @@ typedef struct s_pix
 	float	y;
 }	t_pix;
 
+typedef struct t_img
+{
+	void	*img;
+	int		depth;
+	int		len;
+	int		end;
+	char	*addr;
+}	t_img;
+
 t_pix	find_pix_coords(int i, int j, float density, float ratio);
+void	my_mlx_pixel_put(t_img *pic, int x, int y, unsigned int color);
 
 #endif
--- sources/screen/my_mlx_pixel_put.c
+++ sources/screen/my_mlx_pixel_put.c
@@ -1,30 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minirt.h                                           :+:      :+:    :+:   */
+/*   my_mlx_pixel_put.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/14 23:05:09 by anemesis          #+#    #+#             */
-/*   Updated: 2022/06/22 15:11:16 by anemesis         ###   ########.fr       */
+/*   Created: 2022/06/22 15:46:58 by anemesis          #+#    #+#             */
+/*   Updated: 2022/06/22 15:47:45 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINIRT_H
-# define MINIRT_H
+#include "../../headers/minirt.h"
 
-# include "../libraries/libft/libft.h"
-# include "../libraries/libmlx/headers/mlx.h"
+void	my_mlx_pixel_put(t_img *pic, int x, int y, unsigned int color)
+{
+	char	*dst;
 
-# include "color.h"
-# include "vector.h"
-# include "screen.h"
-# include "ray.h"
-# include "scene.h"
-# include "intersection.h"
-# include "event.h"
-
-# include <stdio.h>
-# include <math.h>
-
-#endif
+	dst = pic->addr + (y * pic->len + x * (pic->depth / 8));
+	*(unsigned int *)dst = color;
+}
