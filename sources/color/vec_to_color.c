b3d251c7d192cd22824f5c00bdfb94577879c0cf
khabibullet <irek.rifson@gmail.com>
2022-06-26 18:58:38 +0300

    fixing color_to_vec.c

--- sources/color/vec_to_color.c
+++ sources/color/vec_to_color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec_to_color.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enoye <enoye@clown.ru>                     +#+  +:+       +#+        */
+/*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 22:23:10 by enoye             #+#    #+#             */
-/*   Updated: 2022/06/26 18:39:23 by enoye            ###   ########.fr       */
+/*   Updated: 2022/06/26 18:58:02 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ unsigned int	vec_to_color(t_color pix)
 {
 	unsigned int	sum;
 
-	sum = + 0x01 * pix.b;
+	sum = 0x01 * pix.b;
 	sum = 0x01 * pix.b;
 	sum = sum + 0x0100 * pix.g;
 	sum = sum + 0x010000 * pix.r;
