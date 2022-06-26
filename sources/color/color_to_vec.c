f8780dc9b68a1d5993b50beec8db1fe6f0e41bfa
Evelynn Noye <enoye@pr-h1.kzn.21-school.ru>
2022-06-26 11:53:59 +0300

    some changes

--- sources/color/color_to_vec.c
+++ sources/color/color_to_vec.c
@@ -6,7 +6,7 @@
 /*   By: enoye <enoye@clown.ru>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 22:20:22 by enoye             #+#    #+#             */
-/*   Updated: 2022/06/26 11:35:48 by enoye            ###   ########.fr       */
+/*   Updated: 2022/06/26 11:52:12 by enoye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ t_color	color_to_vec(unsigned color)
 {
 	t_color		pix;
 
-	pix.b = color % 0x100;
-	color = color / 0x100;
-	pix.g = color % 0x100;
-	color = color / 0x100;
-	pix.r = color % 0x100;
+	pix.r = color / 0x010000;
+	color = color - pix.r;
+	color.g = sum / 0x000100;
+	color.b = sum - color.g;
 	return (pix);
 }
--- sources/vector/find_reflect_vec.c
+++ sources/vector/find_reflect_vec.c
@@ -6,18 +6,19 @@
 /*   By: enoye <enoye@clown.ru>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 21:59:18 by enoye             #+#    #+#             */
-/*   Updated: 2022/06/26 11:21:54 by enoye            ###   ########.fr       */
+/*   Updated: 2022/06/26 11:49:43 by enoye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../headers/minirt.h"
+#include "../../headers/vector.h"
 
 t_vec	find_reflect_vec(t_vec ray, t_vec norm)
 {
 	float	kof;
 	t_vec	reflect;
 
-	kof = 2 * ((ray.x * norm.x) + (ray.y * norm.y) + (ray.z * norm.z)) / ((norm.x * norm.x) + (norm.y * norm.y) + (norm.z * norm.z));
+	kof = 2 * ((ray.x * norm.x) + (ray.y * norm.y) + (ray.z * norm.z)) / \
+	((norm.x * norm.x) + (norm.y * norm.y) + (norm.z * norm.z));
 	reflect.x = ray.x - kof * norm.x;
 	reflect.y = ray.y - kof * norm.y;
 	reflect.z = ray.z - kof * norm.z;
