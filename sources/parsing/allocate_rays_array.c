9e1e51a6641f326c9e462e55f21aac53297f9268
khabibullet <irek.rifson@gmail.com>
2022-07-02 11:01:14 +0300

    adding fuctions for allocation of buffer arrays

--- sources/parsing/allocate_rays_array.c
+++ sources/parsing/allocate_rays_array.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   allocate_rays.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/23 12:58:36 by anemesis          #+#    #+#             */
-/*   Updated: 2022/06/29 16:12:00 by anemesis         ###   ########.fr       */
+/*   Created: 2022/06/28 19:22:28 by anemesis          #+#    #+#             */
+/*   Updated: 2022/06/29 13:52:58 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-# define PARSING_H
+#include "../../headers/minirt.h"
 
-# include "minirt.h"
+void	alloc_rays_array(t_minirt *rt)
+{
+	int	i;
 
-void	parse_rt(int argc, char **argv, t_minirt *rt);
-void	init_window_and_buffers(t_minirt *rt);
-void	count_objects(t_minirt *rt);
-void	alloc_rays_array(t_minirt *rt);
-void	alloc_objects_arrays(t_minirt *rt);
-void	alloc_tmp_collisions_array(t_minirt *rt);
-
-#endif
+	i = 0;
+	rt->screen.rays = malloc(sizeof(*rt->screen.rays) * rt->screen.heigth);
+	while (i < rt->screen.heigth)
+	{
+		rt->screen.rays[i] = malloc(sizeof(**rt->screen.rays) \
+															* rt->screen.width);
+		i++;
+	}
+}
