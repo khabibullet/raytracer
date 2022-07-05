e11656e3c3e4f57ee865097b8648fcae8d67b64b
khabibullet <irek.rifson@gmail.com>
2022-07-05 15:15:59 +0300

    fixing

---
+++ sources/collision/plane_collision.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane_collision.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/26 13:21:05 by enoye             #+#    #+#             */
+/*   Updated: 2022/07/04 17:07:08 by anemesis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/ray.h"
+#include "../../headers/scene.h"
+#include "../../headers/vector.h"
+
+#include <math.h>
+
+int	collide_plane(t_ray *ray, t_plane *plane, t_collision *collision)
+{
+	t_vec	collision;
+	float	t;
+
+	collision = subtract_vecs(ray->origin, plane->coords);
+	t = -dot_product(collision, plane->normal)
+		/ dot_product(ray->coords, plane->normal);
+	if (t <= 0)
+		return (0);
+	collision.x = ray->origin.x + ray->coords.x * t;
+	collision.y = ray->origin.y + ray->coords.y * t;
+	collision.z = ray->origin.z + ray->coords.z * t;
+	// return (collision);
+	return (1);
+}
