0371f91f5e2bfc4456b30df67d126d5fb3f50144
khabibullet <irek.rifson@gmail.com>
2022-08-03 23:29:02 +0300

    fix sphere collision

--- sources/collision/current_ray_collisions.c
+++ sources/collision/current_ray_collisions.c
@@ -6,7 +6,7 @@
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 17:54:34 by anemesis          #+#    #+#             */
-/*   Updated: 2022/08/03 21:05:54 by anemesis         ###   ########.fr       */
+/*   Updated: 2022/08/03 23:28:14 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,15 @@ void	current_ray_nearest_collision(t_ray *ray, t_scene *scene)
 {
 	int	i;
 
-	// printf("cyl center: (%f, %f, %f)\n", scene->cylinders[0].center.x, scene->cylinders[0].center.y, scene->cylinders[0].center.z);
-	// printf("cyl axis: (%f, %f, %f)\n\n", scene->cylinders[0].axis.x, scene->cylinders[0].axis.y, scene->cylinders[0].axis.z);
-	// printf("origin: (%f, %f, %f)\n", ray->origin.x, ray->origin.y, ray->origin.z);
-	// printf("coords: (%f, %f, %f)\n\n", ray->coords.x, ray->coords.y, ray->coords.z);
 	i = -1;
 	while (++i < scene->num_of.cylinders)
 		collide_cylinder(ray, &scene->cylinders[i]);
-	// i = -1;
-	// while (++i < scene->num_of.spheres)
-	// 	collide_sphere(ray, &scene->spheres[i]);
-	// i = -1;
-	// while (++i < scene->num_of.planes)
-	// 	collide_plane(ray, &scene->planes[i]);
+	i = -1;
+	while (++i < scene->num_of.spheres)
+		collide_sphere(ray, &scene->spheres[i]);
+	i = -1;
+	while (++i < scene->num_of.planes)
+		collide_plane(ray, &scene->planes[i]);
 	// i = -1;
 	// while (++i < scene->num_of.polygons)
 	// 	collide_poly(ray, &scene->polygons[i]);
