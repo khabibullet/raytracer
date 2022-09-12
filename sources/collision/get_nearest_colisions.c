bef23106e9f3ce25c2f1d82e10ce55843ef2fb4f
khabibullet <irek.rifson@gmail.com>
2022-09-12 20:39:14 +0300

    fix diffuse

--- sources/collision/get_nearest_colisions.c
+++ sources/collision/get_nearest_colisions.c
@@ -6,7 +6,7 @@
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 21:26:38 by anemesis          #+#    #+#             */
-/*   Updated: 2022/09/12 20:04:14 by anemesis         ###   ########.fr       */
+/*   Updated: 2022/09/12 20:06:52 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	get_first_collisions(t_ray **ray, t_scene *scene, int heigth, int width)
 {
 	register int	i;
 	register int	j;
-	// t_color			color;
 
 	i = 0;
 	while (i < heigth)
@@ -27,11 +26,6 @@ void	get_first_collisions(t_ray **ray, t_scene *scene, int heigth, int width)
 		while (j < width)
 		{
 			current_ray_nearest_collision(&ray[i][j], scene, FULL, i, j);
-			// if (i == 10 && j == 10)
-			// {
-			// 	// color = *(t_color *)&ray[i][j].collis.surface
-			// 	printf("%p\n", ray[i][j].collis.surface);
-			// }
 			++j;
 		}
 		++i;
--- sources/vector/cross_product.c
+++ sources/vector/cross_product.c
@@ -6,7 +6,7 @@
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 17:17:02 by anemesis          #+#    #+#             */
-/*   Updated: 2022/09/12 16:15:05 by anemesis         ###   ########.fr       */
+/*   Updated: 2022/09/12 20:29:57 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ inline t_vec	cross_product(t_vec *vec1, t_vec *vec2)
 {
 	t_vec	prod;
 
-	prod.x = vec1->y * vec2->z - vec1->z * vec2->y;
-	prod.y = vec1->z * vec2->x - vec1->x * vec2->z;
-	prod.z = vec1->x * vec2->y - vec1->y * vec2->x;
+	prod.x = -vec1->y * vec2->z + vec1->z * vec2->y;
+	prod.y = -vec1->z * vec2->x + vec1->x * vec2->z;
+	prod.z = -vec1->x * vec2->y + vec1->y * vec2->x;
 	return (prod);
 }
